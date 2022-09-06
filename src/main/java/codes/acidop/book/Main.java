d67c5d08a5906e3863942a4411d17f56cee0e418
BuildTools <unconfigured@null.spigotmc.org>
2022-09-07 00:43:24 +0530

    Begin

---
+++ src/main/java/codes/acidop/book/Main.java
@@ -0,0 +1,13 @@
+package codes.acidop.book;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        System.out.println("Sacred Book has been enabled!");
+
+    }
+}
